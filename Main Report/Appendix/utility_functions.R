09bda18a94803dc909f42fd9adca142d56d33e39
abot3 <botelho3@illinois.edu>
2021-08-04 01:37:29 -0700

    Add section headers to report. Add code to import dataframe. Add util functions to free memory & select random rows from a data frame.

--- Main Report/Appendix/utility_functions.R
+++ Main Report/Appendix/utility_functions.R
@@ -2,7 +2,7 @@
 
 # plot fitted vs residuals
 plot_fitted_resid = function(model, pointcol = "dodgerblue", linecol = "darkorange") {
-  plot(fitted(model), resid(model), 
+  plot(fitted(model), resid(model),
        col = pointcol, pch = 20, cex = 1.5,
        xlab = "Fitted", ylab = "Residuals")
   abline(h = 0, col = linecol, lwd = 2)
@@ -20,6 +20,20 @@ cal_rmse = function(model){
 }
 
 # calculate leave one out cross validation
-calc_loocv_rmse = function(model) { 
+calc_loocv_rmse = function(model) {
   sqrt(mean((resid(model) / (1 - hatvalues(model))) ^ 2))
+}
+
+# Delete large object obj, call the garbage collector to trigger garbage
+# collector to clean up deleted obj.
+free_memory = function(obj = data.frame()) {
+  rm(obj)
+  gc()
+}
+
+# Select a % of random rows in the data frame. Rows are chosen using a uniform
+# distribution.
+random_rows_from_df = function(df = data.frame(), pct = 0.10) {
+  rowidx = as.integer(runif(round(nrow(df) * 0.10), min = 0, max = nrow(df)))
+  df[rowidx, ]
 }
